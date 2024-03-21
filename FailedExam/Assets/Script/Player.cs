dbf346f0644dbcf5e06582db48a43e4789f6c1c7
SidereaH <hutornoyaa@gmail.com>
2024-03-21 22:44:21 +0300

    ReworkMoveSystem, add own joystick without design, remove joystickPack from unity assets store

--- FailedExam/Assets/Script/Player.cs
+++ FailedExam/Assets/Script/Player.cs
@@ -1,40 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public ControlType controlType;
-    public Joystick joystick;
-    public enum ControlType {PC, Android}
-
+    
+    [SerializeField]
     public float speed;
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Vector2 moveVelocity;
-    void Start()
+    /*void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
+       
+    }*/
+    /*
     // Update is called once per frame
     void Update()
     {
-        if (controlType == ControlType.PC) 
-        { 
-            moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        }
-        else if (controlType == ControlType.Android)
-        {
-            moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
-        }
-        
-        
-        moveVelocity = moveInput.normalized * speed;
-    }
-    private void FixedUpdate()
+      if (controlType == ControlType.PC) 
+       { 
+           moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       }
+       else if (controlType == ControlType.Android)
+       {
+           moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
+       }
+
+
+       moveVelocity = moveInput.normalized * speed;
+   }*/
+    private void Awake()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        rb = GetComponent<Rigidbody2D>();
+    }
+    /* private void FixedUpdate()
+     {
+         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+     }*/
+    private void Update()
+    {
+        rb.velocity = moveInput * speed;
+    }
+    private void OnMove(InputValue inputValue)
+    {
+        moveInput = inputValue.Get<Vector2>();
     }
 }
