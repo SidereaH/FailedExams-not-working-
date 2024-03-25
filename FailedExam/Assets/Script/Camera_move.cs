49e89ce3d5a181bb48e73a4f2eedb1c5b43a93e7
SidereaH <hutornoyaa@gmail.com>
2024-03-25 18:11:54 +0300

    addColisionOffset(smooth going through objects

--- FailedExam/Assets/Script/Camera_move.cs
+++ FailedExam/Assets/Script/Camera_move.cs
@@ -14,6 +14,6 @@ public class Camera_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, -10f), Time.deltaTime * 2f);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, -1f), Time.deltaTime * 2f);
     }
 }
--- FailedExam/Assets/Script/Player.cs
+++ FailedExam/Assets/Script/Player.cs
@@ -16,6 +16,11 @@ public class Player : MonoBehaviour
     private Vector2 moveVelocity;
     private Vector2 smoothMovementInput;
     private Vector2 movementInputSmoothVelocity;
+    private bool facingRight = true;
+    public ContactFilter2D movementFilter;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    public float collisionOffset = 0.5f;
+    
     /*void Start()
     {
        
@@ -44,10 +49,34 @@ public class Player : MonoBehaviour
      {
          rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
      }*/
+    private void Update()
+    {
+        /*if (!facingRight && moveInput.x > 0)
+        {
+            Flip();
+        }
+        else if (facingRight && moveInput.x > 0) 
+        {
+            Flip();
+        }*/
+    }
     private void FixedUpdate()
     {
         SetPlayerVelocity();
-        RotateInDirectionOfInput();
+       // RotateInDirectionOfInput();
+       if(moveInput != Vector2.zero)
+        {
+            bool success = TryMove(moveInput);
+            if (!success)
+            {
+                success = TryMove(new Vector2(moveInput.x,  0));
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, moveInput.y));
+                } 
+            }
+
+        }
 
     }
     private void SetPlayerVelocity()
@@ -57,7 +86,7 @@ public class Player : MonoBehaviour
         rb.velocity = smoothMovementInput * speed;
 
     }
-    private void RotateInDirectionOfInput()
+    /*private void RotateInDirectionOfInput()
     {
         if(moveInput != Vector2.zero)
         {
@@ -65,9 +94,33 @@ public class Player : MonoBehaviour
             Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
             rb.MoveRotation(rotation);
         }
-    }
+    }*/
     private void OnMove(InputValue inputValue)
     {
         moveInput = inputValue.Get<Vector2>();
+    }
+    /*private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }*/
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(
+            direction,
+            movementFilter,
+            castCollisions,
+            speed * Time.fixedDeltaTime + collisionOffset);
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
